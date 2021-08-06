691b373067f01b5d9514e1dbbe60b66a24657afb
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2021-08-06 16:07:41 -0700

    finished hw 7

--- src/edu/miracosta/cs113/MorseCodeTree.java
+++ src/edu/miracosta/cs113/MorseCodeTree.java
@@ -12,22 +12,50 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class MorseCodeTree extends BinaryTree<Character> {
-	BinaryTree<Character> tree;
-	Scanner input;
+	
 
 	public MorseCodeTree() {
-		try {
-			// Variable Set Up
-			input = new Scanner(new File("MorseCodeList.txt"));
-			String line, morseCode;
-			char letter, currentSymbol;
-			BinaryTree<Character> currentTree = new BinaryTree<Character>();
-			tree = new BinaryTree<Character>(null, new BinaryTree<Character>(), new BinaryTree<Character>());
-			currentTree = tree;
+		super(new Node());
 
+		readMorseCodeTree();
+	}
+
+	public void readMorseCodeTree() {
+		try {
+			Scanner scan = new Scanner(new File("./src/edu/miracosta/cs113/morsecodefile.txt"));
+
+			String line;
+			String[] data;
+			Node currentNode;
+
+			while (scan.hasNext()) {
+				currentNode = root;
+				line = scan.nextLine();
+				data = line.split(" ");
+
+				for (int i = 0; i < data[1].length(); i++) {
+					if (data[1].charAt(i) == '*') {
+						if (currentNode.left == null) {
+							currentNode.left = new Node();
+						}
+						currentNode = currentNode.left;
+					} else {
+						if (currentNode.right == null) {
+							currentNode.right = new Node();
+						}
+						currentNode = currentNode.right;
+					}
+				}
+
+				currentNode.data = data[0].charAt(0);
+
+			}
+			scan.close();
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
+
 
 
 	// TODO:
@@ -51,36 +79,36 @@ public class MorseCodeTree extends BinaryTree<Character> {
 	 *
 	 * @param morseCode The given input representing letters in Morse code
 	 * @return a String representing the decoded values from morseCode
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public String translateFromMorseCode(String morseCode) throws Exception {
-		 String[] words = morseCode.split(" ");
-	        Node<Character> currentNode;
-	        StringBuilder sb = new StringBuilder();
+		String[] words = morseCode.split(" ");
+		Node<Character> currentNode;
+		StringBuilder sb = new StringBuilder();
 
-	        //loops through the word array for every word in it
-	        for (String word : words) {
-	            //start from the top of the tree
-	            currentNode = root;
+		// loops through the word array for every word in it
+		for (String word : words) {
+			// start from the top of the tree
+			currentNode = root;
 
-	            //loop through ever character 
-	            for (int i = 0; i < word.length(); i++) {
-	                //check if * or  -
-	                if (word.charAt(i) == '*') {
-	                    // * : traverse left
-	                    currentNode = currentNode.left;
-	                } else if (word.charAt(i) == '-') {
-	                    //  - :traverse right
-	                    currentNode = currentNode.right;
-	                } else {
-	                    //if neither throw an exception
-	                    throw new Exception(word.charAt(i) + "Not valid ");
-	                }
-	            }
-	            //once done traversing, add the letter to the string builder
-	            sb.append(currentNode.data);
-	        }
-	        return sb.toString();
-	    }
+			// loop through ever character
+			for (int i = 0; i < word.length(); i++) {
+				// check if * or -
+				if (word.charAt(i) == '*') {
+					// * : traverse left
+					currentNode = currentNode.left;
+				} else if (word.charAt(i) == '-') {
+					// - :traverse right
+					currentNode = currentNode.right;
+				} else {
+					// if neither throw an exception
+					throw new Exception(word.charAt(i) + "Not valid ");
+				}
+			}
+			// once done traversing, add the letter to the string builder
+			sb.append(currentNode.data);
+		}
+		return sb.toString();
+	}
 }
 // End of class MorseCodeTree
